0e22617ff5e8bb5b4c1c13a04f1d5d3c8b0c3e56
Bebnev Anton <bambr.ab@gmail.com>
2019-11-20 15:46:05 +0300

    stub cleaning

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
     parser:  '@typescript-eslint/parser',
     extends: [
-        "@react-native-community"
-        //"plugin:@typescript-eslint/recommended",
+        "@react-native-community",
+        "prettier",
+        "prettier/@typescript-eslint"
     ],
+    plugins: ["@typescript-eslint"],
     parserOptions:  {
         ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
         sourceType: "module",  // Allows for the use of imports
